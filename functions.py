e1756caa10efb30dc3e3c43a82daf698ccb289fd
emin-mustafayev <81103853+emin-mustafayev@users.noreply.github.com>
2021-05-03 01:06:31 +0400

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,17 @@
+def thing():
+    print('Hello Worrd')
+
+thing()
+print('salam')
+
+def hello(ver):
+    if ver == 'es':
+        print('hola')
+    elif ver == 'en':
+        print('hello')
+    else:
+        print('salamm')
+
+hello('es')
+hello('az')
+hello('en')
